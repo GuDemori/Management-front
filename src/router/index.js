646cb90fca99673efe1b916a1cfc2ee424cc5776
LimaAlexandre-Dev <alexandre.budny1@gmail.com>
2025-06-07 10:25:40 -0300

    corrigindo falhas

--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,9 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/pages/login.vue'
 import Fornecedores from '@/pages/fornecedores.vue'
 import Produtos from '@/pages/Produtos.vue'
-import Pedidos from '@/pages/pedidos.vue'
+import Pedidos from '@/pages/Pedidos.vue'
 import Estoque from '@/pages/estoque.vue'
-import Clientes from '@/pages/clientes.vue'
+import Clients from '@/pages/Clients.vue'
 
 const routes = [
   {
@@ -15,7 +15,7 @@ const routes = [
     meta: { layout: 'login' },
   },
   {
-    path: '/clients',
+    path: '/clientes',
     name: 'Clients',
     component: Clients,
   },
@@ -29,6 +29,16 @@ const routes = [
     name: 'Produtos',
     component: Produtos,
   },
+  {
+    path: '/pedidos',
+    name: 'Pedidos',
+    component: Pedidos,
+  },
+  {
+    path: '/estoque',
+    name: 'Estoque',
+    component: Estoque,
+  }
 ]
 
 const router = createRouter({
@@ -37,46 +47,3 @@ const router = createRouter({
 })
 
 export default router
-
-
-// // Composables
-// import { createRouter, createWebHistory } from 'vue-router/auto'
-// import { setupLayouts } from 'virtual:generated-layouts'
-// import { routes } from 'vue-router/auto-routes'
-
-
-// const router = createRouter({
-//   history: createWebHistory(import.meta.env.BASE_URL),
-//   routes: setupLayouts(routes),
-// })
-
-// // Workaround for https://github.com/vitejs/vite/issues/11804
-// router.onError((err, to) => {
-//   if (err?.message?.includes?.('Failed to fetch dynamically imported module')) {
-//     if (!localStorage.getItem('vuetify:dynamic-reload')) {
-//       console.log('Reloading page to fix dynamic import error')
-//       localStorage.setItem('vuetify:dynamic-reload', 'true')
-//       location.assign(to.fullPath)
-//     } else {
-//       console.error('Dynamic import error, reloading page did not fix it', err)
-//     }
-//   } else {
-//     console.error(err)
-//   }
-// })
-
-
-// router.isReady().then(() => {
-//   localStorage.removeItem('vuetify:dynamic-reload')
-// })
-// router.beforeEach((to, from, next) => {
-//   const token = localStorage.getItem('token');
-
-//   if (to.meta.requiresAuth && !token) {
-//     next('/login');
-//   } else {
-//     next();
-//   }
-// });
-
-// export default router
