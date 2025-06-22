b0b3f21d1550fc7d02736112cc6ee532055425f6
LimaAlexandre-Dev <alexandre.budny1@gmail.com>
2025-06-21 23:37:28 -0300

    Correção do token e logout

--- src/stores/auth.js
+++ src/stores/auth.js
@@ -1,24 +1,24 @@
-// import { defineStore } from 'pinia'
-// import axios from 'axios'
+import { defineStore } from 'pinia'
+import axios from 'axios'
 
-// export const useAuthStore = defineStore('auth', {
-//   state: () => ({
-//     token: localStorage.getItem('authToken') || null
-//   }),
-//   getters: {
-//     isLogged: (state) => !!state.token
-//   },
-//   actions: {
-//     async login(email, password) {
-//       const { data } = await axios.post('/api/login', { email, password })
-//       this.token = data.token
-//       localStorage.setItem('authToken', data.token)
-//       axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`
-//     },
-//     logout() {
-//       this.token = null
-//       localStorage.removeItem('authToken')
-//       delete axios.defaults.headers.common['Authorization']
-//     }
-//   }
-// })
+export const useAuthStore = defineStore('auth', {
+  state: () => ({
+    token: localStorage.getItem('authToken') || null
+  }),
+  getters: {
+    isLogged: (state) => !!state.token
+  },
+  actions: {
+    async login(email, password) {
+      const { data } = await axios.post('/api/login', { email, password })
+      this.token = data.token
+      localStorage.setItem('authToken', data.token)
+      axios.defaults.headers.common['Authorization'] = `Bearer ${data.token}`
+    },
+    logout() {
+      this.token = null
+      localStorage.removeItem('authToken')
+      delete axios.defaults.headers.common['Authorization']
+    }
+  }
+})
